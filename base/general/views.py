d7f94e2aff29ca7d2a1042f36cb5a7ce3f89329b
Sinan Kodur <sinankodur24@gmail.com>
2023-11-28 12:42:47 +0530

    v

--- base/general/views.py
+++ base/general/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+n = 7
+c = 0
+while(n):
+    if(n>5):
+        c = c+ n-1
+        n = n-1
+    else:
+        break
+print(n)
+print(c)
