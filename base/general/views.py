1701aa7e0c7a256c8ccebe788de91404c3eb80d6
Sinan Kodur <sinankodur24@gmail.com>
2024-01-04 13:51:07 +0530

    n

--- base/general/views.py
+++ base/general/views.py
@@ -3,9 +3,8 @@ from .models import Products
 
 # Create your views here.
 
-def home(request):
-    return render(request,'index.html')
 
-def add_product(request):
+
+def home(request):
     data = Products.objects.all()
     return render(request,"index.html",{"details" : data})
