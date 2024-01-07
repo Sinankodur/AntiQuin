e24a69f458b7ef22e5abc959f75901495ff59e16
Sinan Kodur <sinankodur24@gmail.com>
2024-01-07 15:56:29 +0530

    m

--- base/general/forms.py
+++ base/general/forms.py
@@ -6,3 +6,27 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = {'username', 'email', 'password1', 'password2'}
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'form-control',
+        'id': 'username'
+    }))
+    
+    email = forms.CharField(widget=forms.EmailField(attrs={
+        'placeholder': 'Your email address',
+        'class' : 'form-control',
+        'id' : 'email'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class' : 'form-control',
+        'id' : 'email'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm your password',
+        'class' : 'form-control',
+        'id' : 'email'
+    }))
