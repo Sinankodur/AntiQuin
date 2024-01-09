431fb8365902e893fc17b2eeec7dd6605d3067bd
Sinan Kodur <sinankodur24@gmail.com>
2024-01-09 14:44:55 +0530

    m

--- base/item/views.py
+++ base/item/views.py
@@ -8,4 +8,10 @@ def detail(request,pk):
     product = Products.objects.get(pk=pk)
     category = Category.objects.all()
 
-    return render(request, 'details.html', {'product': product, 'category': category})
+    related_items = Products.objects.filter(category=product.category, is_sold = False).exclude(pk=pk)[0:4]
+
+    return render(request, 'details.html', {
+        'product': product, 
+        'category': category,
+        "related_items": related_items
+    })
