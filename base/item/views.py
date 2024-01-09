8ea556ea5d366777f2d2ec1d72a170c64fedadc4
Sinan Kodur <sinankodur24@gmail.com>
2024-01-09 10:03:13 +0530

    m

--- base/item/views.py
+++ base/item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Products
 
 # Create your views here.
-app_name = 'item'
+
 
 def detail(request,pk):
     product = get_object_or_404(Products, pk=pk)
