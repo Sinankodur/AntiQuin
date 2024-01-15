f416a12140bb6aa6dc3988f02db62c7fab4443d2
Sinan Kodur <sinankodur24@gmail.com>
2024-01-15 15:30:14 +0530

    day 13

---
+++ antiquin/static/js/script.js
@@ -0,0 +1,17 @@
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('productId:',productId, 'action:',action);
+
+        console.log('USER:', user);
+        if (user === 'AnonymousUser') {
+            console.log('Not logged')
+        }
+        else{
+            console.log('User is logged in , sending data..')
+        }
+    })
+}
