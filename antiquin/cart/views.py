20b75220cb7c375806a124184f75159838e16b2e
Sinan Kodur <sinankodur24@gmail.com>
2024-01-16 19:12:44 +0530

    day 14

--- antiquin/cart/views.py
+++ antiquin/cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Cart, CartItem
+from item.models import Product
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-# Create your views here.
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return JsonResponse({'message': 'Product added to cart successfully'})
+
+@login_required
+def view_cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_items = cart.cartitem_set.all()
+
+    return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
