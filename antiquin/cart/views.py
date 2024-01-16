e402da6155d2a18fa12746c03af6ac187764f6d9
Sinan Kodur <sinankodur24@gmail.com>
2024-01-16 22:11:19 +0530

    day 14

--- antiquin/cart/views.py
+++ antiquin/cart/views.py
@@ -15,13 +15,17 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return render(request, 'cart/add_confirm.html', {
+    return redirect('/cart/view_cart', {
         'categories' : categories,
     })
 
 @login_required
 def view_cart(request):
-    cart, created = Cart.objects.get_or_create(user=request.user)
-    cart_items = cart.cartitem_set.all()
+    # cart, created = Cart.objects.get_or_create(user=request.user)
+    # cart_items = cart.cartitem_set.all()
 
-    return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
+    # return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
+    cart_items = CartItem.objects.all()
+    total = sum(item.product.price * item.quantity for item in cart_items)
+
+    return render(request, 'cart/view_cart.html', {'cart_items': cart_items, 'total': total})
