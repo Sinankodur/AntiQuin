9afb5e2f630708843686e379ea7fd717bf7f03e3
Sinan Kodur <sinankodur24@gmail.com>
2024-01-16 21:33:30 +0530

    day 14

--- antiquin/cart/views.py
+++ antiquin/cart/views.py
@@ -13,7 +13,7 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return render(request, 'cart/add_confirm.html')
+    return redirect('//')
 
 @login_required
 def view_cart(request):
