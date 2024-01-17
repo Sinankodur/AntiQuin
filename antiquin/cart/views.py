76e9c0d1da9ddf35f93ed60c76824f03b4e1ef41
Sinan Kodur <sinankodur24@gmail.com>
2024-01-17 10:12:40 +0530

    day 15

--- antiquin/cart/views.py
+++ antiquin/cart/views.py
@@ -9,23 +9,23 @@ def add_to_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-    categories = Category.objects.all()
 
     if not created:
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect('/cart/view_cart', {
-        'categories' : categories,
-    })
+    return redirect('/cart/view_cart')
 
 @login_required
 def view_cart(request):
-    # cart, created = Cart.objects.get_or_create(user=request.user)
-    # cart_items = cart.cartitem_set.all()
-
-    # return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
     cart_items = CartItem.objects.all()
     total = sum(item.product.price * item.quantity for item in cart_items)
+    categories= Category.objects.all()
+    product = Product.objects.all()
 
-    return render(request, 'cart/view_cart.html', {'cart_items': cart_items, 'total': total})
+    return render(request, 'cart/view_cart.html', {
+        'cart_items': cart_items,
+        'total': total,
+        'categories' : categories,
+        'product' : product
+    })
--- antiquin/core/views.py
+++ antiquin/core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from item.models import Category, Product
+from cart.models import CartItem
 from .forms import SignupForm
 
 
 def home(request):
     categories = Category.objects.all()
     products = Product.objects.filter(is_sold=False)[0:8]
+    cart_items = CartItem.objects.all()
+    total = sum(item.product.price * item.quantity for item in cart_items)
 
     return render(request,'core/index.html',{
         'categories' : categories,
         'products' : products,
+        'total' : total,
     })
 
 def sign_up(request):
