73db97742f01f9a25f2bbab381413b781323144d
Sinan Kodur <sinankodur24@gmail.com>
2024-01-17 12:12:38 +0530

    day 15

--- antiquin/cart/models.py
+++ antiquin/cart/models.py
@@ -5,10 +5,12 @@ from item.models import Product
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='CartItem')
-    
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveBigIntegerField(default=1)
 
+    def __str__(self):
+        return self.product
