510b008ee5110c9f64996d89b068f34115320496
Sinan Kodur <sinankodur24@gmail.com>
2024-01-20 13:09:24 +0530

    day 18

--- antiquin/core/views.py
+++ antiquin/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 from item.models import Category, Product
 from cart.models import CartItem, Cart
@@ -8,15 +9,35 @@ from favourites.models import Favourite
 from .forms import SignupForm
 
 
-def home(request):
-    categories = Category.objects.all()
-    products = Product.objects.filter(is_sold=False)[0:8]
-    user = request.user
+# def home(request):
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(is_sold=False)[0:8]
+#     user = request.user
     
-    if not user.is_authenticated():
-        user = User.objects.get(name="AnonUser")
+#     cart = Cart.objects.get(user=user)
+#     cart_items = CartItem.objects.filter(cart=cart)
+#     product_count = cart_items.count()
+#     total = sum(item.product.price * item.quantity for item in cart_items)
 
-    else:
+#     fav_count = Favourite.objects.filter(user=user).count()
+
+#     return render(request,'core/index.html',{
+#       'categories' : categories,
+#       'products' : products,
+#       'total' : total,
+#       'product_count' : product_count,
+#       'fav_count' : fav_count,
+#     })
+
+from django.http import HttpResponse
+def home(request):
+    if request.user.is_authenticated:
+        user_id = request.user.id
+
+        categories = Category.objects.all()
+        products = Product.objects.filter(is_sold=False)[0:8]
+        user = request.user
+        
         cart = Cart.objects.get(user=user)
         cart_items = CartItem.objects.filter(cart=cart)
         product_count = cart_items.count()
@@ -30,6 +51,13 @@ def home(request):
         'total' : total,
         'product_count' : product_count,
         'fav_count' : fav_count,
+        })
+    else:
+        categories = Category.objects.all()
+        products = Product.objects.filter(is_sold=False)[0:8]
+        return render(request,'core/index.html',{
+            'categories' : categories,
+            'products' : products,
         })
 
 
@@ -48,4 +76,3 @@ def sign_up(request):
 
 def success(request):
     return render(request, 'core/success.html')
-
