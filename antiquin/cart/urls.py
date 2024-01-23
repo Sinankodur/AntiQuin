2b947e736df5c0c9591307a4759d296a8754e7e4
Sinan Kodur <sinankodur24@gmail.com>
2024-01-23 12:27:02 +0530

    day 24

--- antiquin/cart/urls.py
+++ antiquin/cart/urls.py
@@ -5,7 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('add_to_cart/<int:product_id>',views.add_to_cart, name='add_to_cart'),
-    path('view_cart/',views.view_cart, name="view_cart"),
+    path('',views.view_cart, name="view_cart"),
     path('delete-item/<int:item_id>/',views.delete_item, name='delete_item'),
     path('update-cart/<int:product_id>/',views.update_cart, name='update_cart')
 ]
