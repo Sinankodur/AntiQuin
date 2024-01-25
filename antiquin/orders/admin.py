159863a09dd4ad556f492d0b0a803ec7b94baae5
Sinan Kodur <sinankodur24@gmail.com>
2024-01-25 23:27:20 +0530

    day 23

--- antiquin/orders/admin.py
+++ antiquin/orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-# from .models import Order, OrderItem
-# # Register your models here.
+from .models import Order, OrderItem
 
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- antiquin/orders/urls.py
+++ antiquin/orders/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    
+    path('start-order/',views.start_order, name='start_order')
 ]
