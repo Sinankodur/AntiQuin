fd0c35161bc86f6f63ae21c34d7838373766b989
Sinan Kodur <sinankodur24@gmail.com>
2024-01-25 13:36:43 +0530

    day 23

--- antiquin/account/urls.py
+++ antiquin/account/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('/',views.account,name='account')
+    path('',views.account,name='account'),
+    path('add-address/',views.add_address, name='add_address'),
+
 ]
--- antiquin/account/views.py
+++ antiquin/account/views.py
@@ -30,3 +30,24 @@ def account(request):
         'fav_count': fav_count,
     })
 
+def add_address(request):
+    user = request.user
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    user_cart, created = Cart.objects.get_or_create(user=user)
+
+    cart_items = CartItem.objects.filter(cart=user_cart)
+    product_count = cart_items.count()
+    total = sum(item.product.price * item.quantity for item in cart_items)
+
+    favourites = Favourite.objects.filter(user=user)
+    fav_count = favourites.count()
+
+    return render(request, 'account/delivery_contact.html', {
+        'categories': categories,
+        'products': products,
+        'total': total,
+        'product_count': product_count,
+        'fav_count': fav_count,
+    })
