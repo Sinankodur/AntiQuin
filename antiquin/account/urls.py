1843786e53541e3e1a80f59c55a209828a9ec887
Sinan Kodur <sinankodur24@gmail.com>
2024-02-08 22:21:02 +0530

    re day 3

--- antiquin/account/urls.py
+++ antiquin/account/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('orders/',views.orders_page, name='orders_page'),
     path('orders-details/<int:pk>/',views.orders_details, name='orders_details'),
     path('order-delivered/<int:order_id>',views.deliver_order, name='deliver_order'),
-    path('delivered/',views.delivered, name='delivered')
+    path('delivered/',views.delivered, name='delivered'),
+    path('purchased/',views.delivered_customer, name='delivered_customer')
 ]
--- antiquin/account/views.py
+++ antiquin/account/views.py
@@ -1,75 +1,69 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
 from item.models import Category, Product
 from cart.models import Cart, CartItem
 from favourites.models import Favourite
 from order.models import Order, OrderItem
 
-
-
-@login_required
-def account(request):
-    user = request.user
+def get_common_context(user):
     categories = Category.objects.all()
     products = Product.objects.all()
-
     user_cart, created = Cart.objects.get_or_create(user=user)
-
     cart_items = CartItem.objects.filter(cart=user_cart)
     product_count = cart_items.count()
     total = sum(item.product.price * item.quantity for item in cart_items)
-
     favourites = Favourite.objects.filter(user=user)
     fav_count = favourites.count()
 
-    orders = Order.objects.filter(user=user)
-
-
-    undelivered_orders = Order.objects.filter(is_delivered=False)
-    order_items = OrderItem.objects.filter(order__in=undelivered_orders)
-
-
-    return render(request, 'account/profile.html', {
+    return {
         'categories': categories,
         'products': products,
         'total': total,
         'product_count': product_count,
         'fav_count': fav_count,
-        'orders' : orders,
-        'order_items' : order_items,
-    })
+    }
+
+@login_required
+def account(request):
+    user = request.user
+    orders = Order.objects.filter(user=user)
+    undelivered_orders = Order.objects.filter(is_delivered=False)
+    order_items = OrderItem.objects.filter(order__in=undelivered_orders)
+
+    context = get_common_context(user)
+    context.update({'orders': orders, 'order_items': order_items})
+
+    return render(request, 'account/profile.html', context)
 
 def edit_account(request):
+    user = request.user
+    context = get_common_context(user)
+
     if request.method == 'POST':
-        user = request.user
         user.first_name = request.POST.get('first_name')
         user.last_name = request.POST.get('last_name')
         user.username = request.POST.get('username')
         user.email = request.POST.get('email')
         user.save()
-
         return redirect('/account/')
-    return render(request, 'account/edit_account.html')
+
+    return render(request, 'account/edit_account.html',context)
 
 def users_page(request):
     users = User.objects.all()
-
-    return render(request, 'account/users.html', {
-        'users' : users
-    })
+    return render(request, 'account/users.html', {'users': users})
 
 def orders_page(request):
     user = request.user
+    orders = Order.objects.filter(user=user)
     undelivered_orders = Order.objects.filter(is_delivered=False)
     order_items = OrderItem.objects.filter(order__in=undelivered_orders)
-    orders = Order.objects.filter(user=user)
 
-    return render(request, 'account/orders.html', {
-        'order_items' : order_items,
-        'orders' : orders
-    })
+    context = get_common_context(user)
+    context.update({'orders': orders, 'order_items': order_items})
+
+    return render(request, 'account/orders.html', context)
 
 def orders_details(request, pk):
     orders = Order.objects.filter(pk=pk)
@@ -85,14 +79,24 @@ def deliver_order(request, order_id):
     if not order.is_delivered:
         order.mark_as_delivered()
         order.save()
-      
         return redirect('/account/delivered')
 
-    return render(request, 'account/orders.html')
+    return redirect('/account/orders')
 
 def delivered(request):
     orders = Order.objects.filter(is_delivered=True)
+    context = get_common_context(request.user)
+    context.update({'orders': orders})
+    return render(request, 'account/delivered.html', context)
 
-    return render(request, 'account/delivered.html', {
-        'orders' : orders
-    })
+def delivered_customer(request):
+    user = request.user
+    orders = Order.objects.filter(is_delivered=True, user=user)
+
+    context = get_common_context(user)
+    context.update({'orders': orders})
+
+    return render(request, 'account/delivered.html', context)
+
+# def delete_user(request):
+    
