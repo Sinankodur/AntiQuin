d5c42ece739cf36900946d530862714abc4c8b42
sinan kodur <sinankodur24@gmail.com>
2024-03-14 10:37:58 +0530

    done

--- antiquin/item/urls.py
+++ antiquin/item/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('detail/<int:pk>/', views.detail, name='detail'),
     path('search/', views.searchProduct, name='search'), 
     path('new/', views.add_items,name='new'),
-    path('edit/<int:pk>', views.edit, name='edit'),
+    path('edit/<int:pk>',views.edit, name='edit'),
     path('delete/<int:pk>', views.delete, name='delete'),
 ]
