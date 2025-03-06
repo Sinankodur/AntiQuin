8ceb11b59552e83d1dc4eef3c0bd1c794d823d9d
sinan kodur <sinankodur24@gmail.com>
2025-03-06 09:45:42 +0530

    some formatting

--- antiquin/cart/views.py
+++ antiquin/cart/views.py
@@ -21,12 +21,11 @@ def add_to_cart(request, product_id):
 
     return redirect('/cart/')
 
+
 @login_required
 def update_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    
     cart = Cart.objects.get(user=request.user)
-    
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
 
     if request.method == 'POST':
@@ -46,12 +45,10 @@ def update_cart(request, product_id):
 def view_cart(request):
     cart_items = None
     favourites = None
-    
     if request.user.is_authenticated:
         user = request.user
 
         cart, created = Cart.objects.get_or_create(user=user)
-        
         cart_items = CartItem.objects.filter(cart=cart)
         product_count = CartItem.objects.filter(cart=cart).count()
         total = sum(item.product.price * item.quantity for item in cart_items)
@@ -67,10 +64,10 @@ def view_cart(request):
     return render(request, 'cart/view_cart.html', {
         'cart_items': cart_items,
         'total': total,
-        'categories' : categories,
-        'product' : product,
-        'product_count' : product_count,
-        'fav_count' : fav_count,
+        'categories': categories,
+        'product': product,
+        'product_count': product_count,
+        'fav_count': fav_count,
     })
 
 
